7e58fa49a75ced9dee0d6a4ee91beac2c95112de
whelxks <70426064+whelxks@users.noreply.github.com>
2021-08-03 13:46:15 +0800

    solved errors

--- src/sharedcomponents/BottomNav.tsx
+++ src/sharedcomponents/BottomNav.tsx
@@ -6,34 +6,20 @@ import "../index.css";
 import { Bottomstuffs } from "./bottomstuffs";
 import { IconContext } from "react-icons";
 
-
-
 function Navbar() {
-
-
   return (
-
-    
-        <div className="flexbox-container">
-          
-           
-            {Bottomstuffs.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path} className="bottom-container" >
-                   
-                    {item.icon}
-                    {item.title}
-                    
-                  </Link>
-                </li>
-              );
-            
-              })}
-          
-          </div>
-       
-      
+    <div className="flexbox-container">
+      {Bottomstuffs.map((item, index) => {
+        return (
+          <li key={index} className={item.cName}>
+            <Link to={item.path} className="bottom-container">
+              {item.icon}
+              {item.title}
+            </Link>
+          </li>
+        );
+      })}
+    </div>
   );
 }
 
