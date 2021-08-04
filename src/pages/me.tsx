23c7057660827d5a901794cd70222cf912b5afab
eugene teu <eugeneteu@gmail.com>
2021-08-04 22:27:04 +0800

    number of vouchers

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -23,7 +23,7 @@ export const Me: React.FC<{}> = () => {
           <div
             style={{
               alignItems: 'center',
-              justifyContent: 'start',
+              justifyContent: 'flex-start',
               display: 'flex',
             }}
           >
@@ -31,8 +31,36 @@ export const Me: React.FC<{}> = () => {
               style={{ fontSize: '2rem' }}
               className="site-form-item-icon"
             />
-            <div className="header-style" style={{ marginTop: '0%' }}>
+            <div className="header-style" style={{ marginTop: '3%' }}>
               My Stats
+            </div>
+          </div>
+          <div
+            style={{
+              alignItems: 'center',
+              justifyContent: 'start',
+              display: 'flex',
+            }}
+          >
+            <div className="header-style" style={{ marginTop: '0%' }}>
+              Email:
+            </div>
+            <div className="mid-header-style" style={{ marginTop: '0%' }}>
+              {user?.email}
+            </div>
+          </div>
+          <div
+            style={{
+              alignItems: 'center',
+              justifyContent: 'start',
+              display: 'flex',
+            }}
+          >
+            <div className="header-style" style={{ marginTop: '0%' }}>
+              No. Of Vouchers:
+            </div>
+            <div className="mid-header-style" style={{ marginTop: '0%' }}>
+              {user?.vouchers.length}
             </div>
           </div>
         </Card>
